2501987edd253017adc4c88e3107bd27743636e1
Khanh Nguyen <khanh.nguyen@members.encacap.com>
2024-08-26 13:11:23 +0000

    chore(eslint): update configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 /** @type {import('@eslint/js').Linter.FlatConfig[]} */
 
 import { fixupPluginRules } from "@eslint/compat";
-import eslint from "@eslint/js";
-import * as reactQueryPlugin from "@tanstack/eslint-plugin-query";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
-import prettierPluginRecommended from "eslint-plugin-prettier/recommended";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import reactRefreshPlugin from "eslint-plugin-react-refresh";
-import sonarjs from "eslint-plugin-sonarjs";
+import pluginJs from "@eslint/js";
+import * as pluginReactQuery from "@tanstack/eslint-plugin-query";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHook from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+import pluginSonarjs from "eslint-plugin-sonarjs";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginTs from "typescript-eslint";
 
 export default [
-  eslint.configs.recommended,
+  pluginJs.configs.recommended,
   {
     files: ["src/**/*.ts", "src/**/*.tsx"],
     languageOptions: {
@@ -28,18 +28,18 @@ export default [
       },
     },
     plugins: {
-      react: fixupPluginRules(reactPlugin),
-      "react-hooks": fixupPluginRules(reactHooksPlugin),
-      "@tanstack/query": fixupPluginRules(reactQueryPlugin),
-      "react-refresh": reactRefreshPlugin,
-      "jsx-a11y": jsxA11yPlugin,
+      react: fixupPluginRules(pluginReact),
+      "react-hooks": fixupPluginRules(pluginReactHook),
+      "@tanstack/query": fixupPluginRules(pluginReactQuery),
+      "react-refresh": pluginReactRefresh,
+      "jsx-a11y": pluginJsxA11y,
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...reactPlugin.configs["jsx-runtime"].rules,
-      ...jsxA11yPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      ...reactQueryPlugin.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs["jsx-runtime"].rules,
+      ...pluginJsxA11y.configs.recommended.rules,
+      ...pluginReactHook.configs.recommended.rules,
+      ...pluginReactQuery.configs.recommended.rules,
       "react/prop-types": "off",
     },
     settings: {
@@ -49,9 +49,9 @@ export default [
     },
   },
   {
-    ignores: [".next/*", "node_modules/*", "*.config.*", "dist/*", "*.cjs", "*.prepare.*"],
+    ignores: ["node_modules/*", "*.config.*", "dist/*", "*.cjs", "*.prepare.*"],
   },
-  ...tseslint.configs.recommended,
-  sonarjs.configs.recommended,
-  prettierPluginRecommended,
+  ...pluginTs.configs.recommended,
+  pluginSonarjs.configs.recommended,
+  pluginPrettierRecommended,
 ];
